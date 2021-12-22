1585d224a4945b9b18e3ed5d5195afa9d11e9843
Ingar <ingar@megarden.no>
2021-12-22 13:41:45 +0100

    Better blacklist flow

--- start_eth.py
+++ start_eth.py
@@ -61,31 +61,44 @@ def getHashRate():
     return hashRate
 
 
-def CheckRunningPrograms():
-
-    processes = psutil.process_iter()
-    blacklist = []
+def BlackList(trex_blacklist_path):
     # Read Blacklist file
-    with open(r"D:\Workspace\eth-start\blacklist.txt", "r") as f:
+    with open(trex_blacklist_path, "r") as f:
         for line in f.readlines():
 
             if line != "" or line != "\n":
                 line = line.strip("\n")
-                blacklist.append(line.lower())
+                logging.debug(line)
+                isRunning = CheckIfRunning(line.lower())
+                if isRunning.lower() in line.lower():
+                    return isRunning
+                else:
+                    return ""
+
+
+def CheckIfRunning(process):
     # Checks if program in blacklist is running
+    processes = psutil.process_iter()
+    logging.debug("--------------------------------------------------------------------------------------")
     for p in processes:
         pName = p.name().lower()
-        if pName in blacklist:
+        
+        logging.debug(pName)
+        
+
+        if process.lower() in pName.lower():
             logging.debug(f"{pName} is running______________________________________________________________")
-            return True
+            return pName
+    return ""
 
 
 def main(trexConf):
     trex_path = trexConf[0]
     trex_config_path = trexConf[1]
+    trex_blacklist_path = trexConf[2]
     logging.debug("Starting")
     idleTimeReq = 10  # Idle time before start in Seconds
-    refreshRate = 3    # How often to check in Seconds
+    refreshRate = 15    # How often to check in Seconds
     mining = False
     msg = False
     while True:
@@ -100,13 +113,18 @@ def main(trexConf):
                     logging.info("Mining already running, not staring")
                     msg = True
         elif getIdleTime() <= idleTimeReq and mining == True:
-            blacklist = CheckRunningPrograms()
-            if blacklist:
-                logging.debug("Program in blacklist running. Stopping mining")
+            blacklist = BlackList(trex_blacklist_path)
+            logging.debug(f"Blacklist = {blacklist}")
+            if blacklist != "":
+                logging.info(f"{blacklist} in blacklist running. Stopping mining")
                 kill(idleTimeReq, False)
                 mining = False
-        logging.info("Waiting {} seconds to check again".format(refreshRate))
+        if mining:
+            logging.info("Is mining, but no blacklisted programs are running. Waiting {} seconds to check again".format(refreshRate))
+        else:
+            logging.info("Waiting {} seconds to check again".format(refreshRate))
         time.sleep(refreshRate)
+
 
 def ReadConfig():
     logging.debug("readConfig")
@@ -118,8 +136,9 @@ def GetTrexConf():
     conf = ReadConfig()
     trex_path = conf["trex_path"]
     trex_config_path = conf["trex_config_path"]
+    trex_blacklist_path = conf["trex_blacklist_path"]
     logging.debug(trex_path)
-    return trex_path, trex_config_path
+    return trex_path, trex_config_path, trex_blacklist_path
 
 
 # Run Script
@@ -128,7 +147,7 @@ if __name__ == "__main__":
     logging.basicConfig(
         format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
         datefmt='%d-%m-%Y:%H:%M:%S',
-        level=logging.DEBUG,
+        level=logging.INFO,
         handlers=[
             logging.FileHandler("start_eth.log"),
             logging.StreamHandler()
